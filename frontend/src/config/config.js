d7e9bc26551e116b6c8c387d013c085c4a450b98
rbr8 <bencsr@freemail.hu>
2015-03-18 22:01:05 +0100

    Add $http interceptor to parse all dates from json responses

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,6 +3,7 @@
 
     angular
         .module('sfinapp.config', [
+            'sfinapp.config.http',
             'sfinapp.config.state'
         ]);
 
---
+++ frontend/src/config/http/http.js
@@ -0,0 +1,53 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('sfinapp.config.http', [
+
+        ])
+        .config(httpConfig);
+
+
+    function httpConfig($httpProvider) {
+        $httpProvider.defaults.transformResponse.push(parseResponseDates);
+    }
+
+
+
+    function parseResponseDates(responseData) {
+        convertDateStringsToDates(responseData);
+        return responseData;
+    }
+
+
+    var dateRegex = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/;
+
+    function convertDateStringsToDates(input) {
+        // Ignore things that aren't objects.
+        if (typeof input !== 'object') {
+            return;
+        }
+
+        for (var key in input) {
+            if (!input.hasOwnProperty(key)) {
+                continue;
+            }
+
+            var value = input[key];
+            var match;
+
+            // Check for string properties which look like dates.
+            if (typeof value === 'string' && (match = value.match(dateRegex))) {
+                var milliseconds = Date.parse(match[0]);
+                if (!isNaN(milliseconds)) {
+                    input[key] = new Date(milliseconds);
+                }
+            } else if (typeof value === 'object') {
+                // Recurse into object
+                convertDateStringsToDates(value);
+            }
+        }
+    }
+
+})();
+
