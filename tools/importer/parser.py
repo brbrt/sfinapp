4d3bfb0520a19854a2eede3791dd6bb22031cea8
rbr8 <bencsr@freemail.hu>
2015-04-05 00:10:55 +0200

    Refactor importer

---
+++ tools/importer/parser.py
@@ -0,0 +1,26 @@
+import csv
+import logging as log
+
+from transaction import *
+
+def parse(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+
+    log.debug('Processing %d rows.', len(rows))
+
+    transactions = []
+
+    for i, row in enumerate(rows):
+        if len(row) == 0:
+             continue
+
+        try:
+            tr = Transaction(row)
+            transactions.append(tr)
+            log.debug(tr)
+        except ParseError as e:
+            log.info('ParseError: "%s", row %s is skipped.', e, i)
+
+    return transactions
