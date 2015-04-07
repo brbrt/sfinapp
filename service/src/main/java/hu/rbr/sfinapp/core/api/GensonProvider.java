2f83250b9d99c33d5b44ee27d2cdc16549591387
rbr8 <bencsr@freemail.hu>
2015-04-07 19:56:15 +0200

    Use genson for JSON serialization

---
+++ service/src/main/java/hu/rbr/sfinapp/core/api/GensonProvider.java
@@ -0,0 +1,29 @@
+package hu.rbr.sfinapp.core.api;
+
+import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import java.text.SimpleDateFormat;
+
+
+@Provider
+public class GensonProvider implements ContextResolver<Genson> {
+
+    public static final String API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+    private final Genson genson;
+
+    public GensonProvider() {
+        genson = new GensonBuilder()
+                .useIndentation(true)
+                .useDateFormat(new SimpleDateFormat(API_DATE_FORMAT))
+                .create();
+    }
+
+    @Override
+    public Genson getContext(Class<?> type) {
+        return genson;
+    }
+}
