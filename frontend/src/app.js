4e120f6e1ea394c4eeb7d403d8331d50f4cb198e
rbr8 <bencsr@freemail.hu>
2015-05-24 18:33:51 +0200

    Improve ui-router error handling

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -22,17 +22,6 @@ angular
     .controller('mainCtrl', mainCtrl);
 
 
-function mainCtrl($rootScope,
-                  $log,
-                  $state,
-                  toastr) {
+function mainCtrl() {
 
-    $log.debug('Sfinapp states', $state.get());
-
-    // Display errors on state changes (e.g. in resolve...)
-    $rootScope.$on('$stateChangeError', function scError(event, toState, toParams, fromState, fromParams, error) {
-        $log.error('$stateChangeError (' + fromState.name + ' -> ' + toState.name + '): ', error);
-
-        toastr.error('$stateChangeError');
-    });
 }
--- frontend/src/config/state/state.js
+++ frontend/src/config/state/state.js
@@ -2,11 +2,36 @@ angular
     .module('sfinapp.config.state', [
         'ui.router'
     ])
-    .config(stateConfig);
+    .config(stateConfig)
+    .run(stateRun);
 
 
 function stateConfig($stateProvider,
                      $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
+}
+
+function stateRun($log,
+                  $rootScope,
+                  toastr) {
+
+    // Display errors on state changes (e.g. in resolve...)
+    $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
+        $log.error('$stateChangeError (' + fromState.name + ' -> ' + toState.name + '): ', error);
+
+        toastr.error(getErrorMessage(error));
+    });
+
+    function getErrorMessage(error) {
+        if (error.status === 502) {
+            return 'Service unavailable.';
+        }
+
+        if (error.data && error.data.message) {
+            return error.data.message;
+        }
+
+        return 'Unknown error.';
+    }
 }
