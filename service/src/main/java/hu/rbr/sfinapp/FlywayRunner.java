459b610bf547e2b04c658286c54aba1010e33409
rbr8 <bencsr@freemail.hu>
2015-05-30 16:14:38 +0200

    Fix Flyway datasource init

--- service/src/main/java/hu/rbr/sfinapp/FlywayRunner.java
+++ service/src/main/java/hu/rbr/sfinapp/FlywayRunner.java
@@ -1,21 +1,21 @@
 package hu.rbr.sfinapp;
 
-import hu.rbr.sfinapp.core.config.Config;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 public class FlywayRunner {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final Config config;
+    private final DataSource dataSource;
 
     @Inject
-    public FlywayRunner(Config config) {
-        this.config = config;
+    public FlywayRunner(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void run() {
@@ -25,7 +25,7 @@ public class FlywayRunner {
         Flyway flyway = new Flyway();
 
         // Point it to the database
-        flyway.setDataSource(config.get("db.url"), config.get("db.username"), config.get("db.password"));
+        flyway.setDataSource(dataSource);
 
         // Start the migration
         flyway.migrate();
