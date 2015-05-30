8363b6f46d066a017842915b0a3e5d8e3c8ef541
rbr8 <bencsr@freemail.hu>
2015-05-30 15:14:09 +0200

    Use HikariCP connection pool
    Closes #2

--- service/src/main/java/hu/rbr/sfinapp/core/db/DataSourceProvider.java
+++ service/src/main/java/hu/rbr/sfinapp/core/db/DataSourceProvider.java
@@ -1,6 +1,7 @@
 package hu.rbr.sfinapp.core.db;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import hu.rbr.sfinapp.core.config.Config;
 
 import javax.inject.Inject;
@@ -18,11 +19,19 @@ public class DataSourceProvider implements Provider<DataSource> {
 
     @Override
     public DataSource get() {
-        MysqlDataSource ds = new MysqlDataSource();
+        HikariDataSource ds = new HikariDataSource();
 
-        ds.setURL(config.get("db.url"));
-        ds.setUser(config.get("db.username"));
-        ds.setPassword(config.get("db.password"));
+        ds.setDataSourceClassName(MysqlDataSource.class.getName());
+        ds.addDataSourceProperty("serverName", config.get("db.server"));
+        ds.addDataSourceProperty("port", config.getInt("db.port"));
+        ds.addDataSourceProperty("databaseName", config.get("db.database_name"));
+        ds.addDataSourceProperty("user", config.get("db.username"));
+        ds.addDataSourceProperty("password", config.get("db.password"));
+
+        ds.setMaximumPoolSize(config.getInt("db.max_pool_size"));
+        ds.addDataSourceProperty("cachePrepStmts", true);
+        ds.addDataSourceProperty("prepStmtCacheSize", 25);
+        ds.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
 
         return ds;
     }
