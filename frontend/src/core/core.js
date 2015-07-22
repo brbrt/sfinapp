2ae3b395023bd1db5d8e22e7bf52819c0cd3b917
rbr8 <bencsr@freemail.hu>
2015-07-22 20:12:27 +0200

    Remove unused components

--- frontend/src/core/core.js
+++ frontend/src/core/core.js
@@ -2,8 +2,6 @@ angular
     .module('sfinapp.core', [
         'sfinapp.core.confirmSrv',
         'sfinapp.core.dateUtilSrv',
-        'sfinapp.core.filterSrv',
-        'sfinapp.core.filterWatch',
         'sfinapp.core.locationSrv',
         'sfinapp.core.stateRef'
     ]);
