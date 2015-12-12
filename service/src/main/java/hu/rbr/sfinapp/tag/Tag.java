0bcd75b462387e7f26fe7545937884464a744cd4
brbrt <bencsr@freemail.hu>
2015-12-12 14:59:20 +0100

    Remove validation constraint from tag entity

--- service/src/main/java/hu/rbr/sfinapp/tag/Tag.java
+++ service/src/main/java/hu/rbr/sfinapp/tag/Tag.java
@@ -1,7 +1,6 @@
 package hu.rbr.sfinapp.tag;
 
 import com.google.common.base.MoreObjects;
-import org.hibernate.validator.constraints.NotBlank;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
@@ -11,12 +10,8 @@ public class Tag implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public Integer id;
-
-    @NotBlank(message = "Name is required!")
     public String name;
-
     public String description;
-
     public Integer parentId;
 
     @Override
