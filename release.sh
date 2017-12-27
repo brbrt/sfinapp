0b982f29b706838f41807e183572ce4ea9df9bd1
brbrt <bencsr@freemail.hu>
2017-12-27 21:05:57 +0100

    Publish result artifact to bintray

--- release.sh
+++ release.sh
@@ -2,12 +2,4 @@
 
 set -ev
 
-./gradlew build
-
-rm -rf release
-mkdir release 
-
-cp sfinapp-backend/build/libs/sfinapp-0.0.1-SNAPSHOT.jar release
-
-cd release
-tar -zcf sfinapp.tar.gz *
+./gradlew build bintrayUpload
