686bde0f5f7dbccd97132598448eabb281175595
brbrt <bencsr@freemail.hu>
2017-12-29 17:37:56 +0100

    Reference jar directly in Dockerfile

--- release.sh
+++ release.sh
@@ -5,5 +5,5 @@ set -ev
 ./gradlew build
 
 cd sfinapp-backend
-docker build --build-arg JAR_FILE=build/libs/sfinapp-1.0.jar -t brbrt/sfinapp .
+docker build -t brbrt/sfinapp .
 docker push brbrt/sfinapp
