23c256170834e8db7f6f9241b32e53925e24eefb
brbrt <bencsr@freemail.hu>
2017-12-30 05:53:49 +0100

    Tidy up release script

--- release.sh
+++ release.sh
@@ -5,12 +5,5 @@ set -ev
 ./gradlew build
 
 cd sfinapp-backend
-
-
-ls -l
-ls -l build/
-ls -l build/libs/
-
-cp build/libs/sfinapp-1.0.jar ./sfinapp.jar
-docker build -t brbrt/sfinapp .
+docker build --build-arg JAR_FILE=build/libs/sfinapp-1.0.jar -t brbrt/sfinapp .
 docker push brbrt/sfinapp
